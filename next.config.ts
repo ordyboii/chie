68c3825c1a8626443661a33ff41fd9db829f0262
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-12-19 09:02:15 +0000

    setup new direction and project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const config: NextConfig = {
+const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default config;
+export default nextConfig;

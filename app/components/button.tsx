3d8e5383f971f36f08f081fdacf5adfa8b139967
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-12-20 19:31:42 +0000

    refactor button components to use variant setup function for improved styling

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,33 +1,50 @@
 import Link, { LinkProps } from "next/link";
 import { HTMLAttributes, PropsWithChildren } from "react";
 
-type Variant = "default" | "secondary" | "danger" | "ghost";
+function setupComponentVariants<T extends Record<string, string>>(params: {
+  defaultClass: string;
+  variants: T;
+}) {
+  const { defaultClass, variants } = params;
+  return function (props: { variant: keyof T; className?: string }) {
+    return `${defaultClass} ${variants[props.variant]} ${props.className}`;
+  };
+}
+
+type InferVariants<T> = T extends (props: { variant: infer V }) => any
+  ? V
+  : never;
+
+const variants = setupComponentVariants({
+  defaultClass: "m-button",
+  variants: {
+    secondary: "m-button--secondary",
+    danger: "m-button--danger",
+    ghost: "m-button--ghost",
+  },
+});
 
 export default function Button(
   props: PropsWithChildren<
     HTMLAttributes<HTMLButtonElement> & {
-      variant: Variant;
+      variant: InferVariants<typeof variants>;
     }
   >
 ) {
   return (
-    <button
-      className={`m-button ${props.className}`}
-      data-variant={props.variant}
-      {...props}
-    >
+    <button className={props.variant} {...props}>
       {props.children}
     </button>
   );
 }
 
-export function InternalLinkButton(props: LinkProps & { variant: Variant }) {
+export function InternalLinkButton(
+  props: PropsWithChildren<
+    LinkProps & { variant: InferVariants<typeof variants> }
+  >
+) {
   return (
-    <Link
-      className={`m-button ${props.className}`}
-      data-variant={props.variant}
-      {...props}
-    >
+    <Link className={props.variant} {...props}>
       {props.children}
     </Link>
   );
@@ -36,16 +53,12 @@ export function InternalLinkButton(props: LinkProps & { variant: Variant }) {
 export function ExternalLinkButton(
   props: PropsWithChildren<
     HTMLAttributes<HTMLAnchorElement> & {
-      variant: Variant;
+      variant: InferVariants<typeof variants>;
     }
   >
 ) {
   return (
-    <a
-      className={`m-button ${props.className}`}
-      data-variant={props.variant}
-      {...props}
-    >
+    <a className={props.variant} {...props}>
       {props.children}
     </a>
   );

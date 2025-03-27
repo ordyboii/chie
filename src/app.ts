4c1e685c54d72f4270a3a96d1078b2c83712506a
ordyboii <68291857+ordyboii@users.noreply.github.com>
2025-03-27 23:44:40 +0000

    feat: removed bullmq and redis

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,25 @@
-import { Queue, Worker } from "bullmq";
-import { slack } from "@common/api";
-import { connection, redis } from "@common/redis";
-import * as NHK from "@features/nhk/service";
+import { schedule } from "node-cron";
+import { readFile } from "fs/promises";
+import { slack } from "@api";
+import * as NHK from "@nhk/service";
 
-const queueName = "send-phrase-to-slack";
-const queue = new Queue(queueName, { connection });
-
-await queue.upsertJobScheduler("every-day", { pattern: "0 17 * * *" });
-// await queue.upsertJobScheduler("every-day", { every: 1000000 });
-
-const worker = new Worker(
-  queueName,
-  async (job) => {
-    console.log(`Job started ${job.name}`);
-    job.updateProgress(10);
-
-    await NHK.sendPhraseToSlack();
-
-    job.updateProgress(100);
-    console.log(`Job ended ${job.name} ${job.id}`);
-  },
-  { connection },
-);
+schedule("* * * * *", async () => {
+  console.log("Job started");
+  await NHK.sendPhraseToSlack();
+  console.log("Job ended");
+});
 
 slack.action("japanese-phrase-translate", async (data) => {
   await data.ack();
   const input =
     data.body.state.values["japanese-phrase"]["japanese-phrase-input"].value;
 
-  const phrase = await redis.get("japanese-phrase");
+  const phrase = await readFile(".cache/japanese-phrase.txt", {
+    encoding: "utf8",
+  });
+
   await NHK.translatePhraseAndSendToSlack({ phrase, input });
 });
-
-console.log(`Worker started ${worker.name}`);
 
 await slack.start();
 slack.logger.info("⚡️ Bolt app is running!");

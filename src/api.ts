ed94a46909be9ed71430718f47dcc812c1ab3cbd
ordyboii <68291857+ordyboii@users.noreply.github.com>
2025-03-29 12:54:34 +0000

    feat: added new language client

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,30 @@
 import bolt from "@slack/bolt";
-import Replicate from "replicate";
 import { env } from "@env";
+import { v2 } from "@google-cloud/translate";
+import { replaceEscapedNewLines } from "@utils";
 
 const { App } = bolt;
 
-export const replicate = new Replicate({
-  auth: env.REPLICATE_API_TOKEN,
-});
+export function createTranslateClient() {
+  return new v2.Translate({
+    credentials: {
+      type: env.GOOGLE_API_TYPE,
+      project_id: env.GOOGLE_API_PROJECT_ID,
+      universe_domain: env.GOOGLE_API_UNIVERSE_DOMAIN,
+      client_id: env.GOOGLE_API_CLIENT_ID,
+      client_email: env.GOOGLE_API_CLIENT_EMAIL,
+      private_key_id: env.GOOGLE_API_PRIVATE_KEY_ID,
+      private_key: replaceEscapedNewLines(env.GOOGLE_API_PRIVATE_KEY),
+    },
+  });
+}
 
-export const slack = new App({
-  token: env.SLACK_BOT_TOKEN,
-  appToken: env.SLACK_APP_TOKEN,
-  socketMode: true,
-  signingSecret: env.SLACK_SIGNING_SECRET,
-  port: 3000,
-});
+export function createBot() {
+  return new App({
+    token: env.SLACK_BOT_TOKEN,
+    appToken: env.SLACK_APP_TOKEN,
+    socketMode: true,
+    signingSecret: env.SLACK_SIGNING_SECRET,
+    port: 3000,
+  });
+}
--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,4 @@ export const NHK_NEWS_URL = "https://www3.nhk.or.jp/news/easy";
 export const NHK_TOP_NEWS_URL =
   "https://www3.nhk.or.jp/news/easy/top-list.json";
 
-export const REPLICATE_MODEL = "meta/meta-llama-3-8b-instruct";
-
 export const PROD = process.env.NODE_ENV === "production";

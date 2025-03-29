f26a177167b8d0f50d911408e8ba8ea51e36ca93
ordyboii <68291857+ordyboii@users.noreply.github.com>
2025-03-29 12:58:54 +0000

    fix: renamed file

---
+++ src/bot.ts
@@ -0,0 +1,24 @@
+import { schedule } from "node-cron";
+import { readFile } from "fs/promises";
+import { sendPhraseToBot, translateAndSendToBot } from "@nhk/service";
+import { bot } from "@utils";
+
+schedule("* * * * *", async () => {
+  console.log("Job started");
+  await sendPhraseToBot();
+});
+
+bot.action("japanese-phrase-translate", async (data) => {
+  await data.ack();
+  const input =
+    data.body.state.values["japanese-phrase"]["japanese-phrase-input"].value;
+
+  const phrase = await readFile(".cache/japanese-phrase.txt", {
+    encoding: "utf8",
+  });
+
+  await translateAndSendToBot({ phrase, input });
+});
+
+await bot.start();
+bot.logger.info("⚡️ Bolt app is running!");

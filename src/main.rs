ef7a82629e0de8ecbcace69f3e626ac32376496a
ordyboii <68291857+ordyboii@users.noreply.github.com>
2025-11-01 15:53:36 +0000

    feat: added axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, routing::get, serve};
+use serde::Serialize;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    serve(listener, app).await.unwrap();
+}
+
+#[derive(Serialize)]
+struct User {
+    name: String,
+}
+
+async fn handler() -> Json<User> {
+    Json(User {
+        name: "hello".to_string(),
+    })
 }

f59929e55d71e0e626335abf5bd022e83e679557
ordyboii <68291857+ordyboii@users.noreply.github.com>
2025-11-01 15:18:32 +0000

    feat: new project

--- src/config/constants.ts
+++
@@ -1,5 +0,0 @@
-export const constants = {
-	NHK_NEWS_URL: "https://www3.nhk.or.jp/news/easy",
-	NHK_TOP_NEWS_URL: "https://www3.nhk.or.jp/news/easy/top-list.json",
-	PROD: process.env.NODE_ENV === "production",
-};
--- src/config/vendor.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "@/config/env";
-import { v2 } from "@google-cloud/translate";
-
-export const googleTranslate = new v2.Translate({
-	credentials: env.GOOGLE_APPLICATION_CREDENTIALS,
-});
